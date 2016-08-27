63917609efdc8e20c3f482751a31869f0f1ed443
Pjotor87 <andersson.peter0@gmail.com>
2016-08-27 10:19:08 +0200

    Initial commit

---
+++ FileTree/Folder.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace FileTree
+{
+    public class Folder
+    {
+        public string Path { get; set; }
+        public string Name { get { return System.IO.Path.GetFileName(this.Path); } }
+        public ICollection<Folder> SubFolders { get; set; }
+        public bool HasSubFolders { get { return this.SubFolders.Count > 0; } }
+        public IEnumerable<string> Files { get; set; }
+        public bool HasFiles { get { return this.Files != null && this.Files.Any(); } }
+
+        public Folder(string folderPath)
+        {
+            if (!Directory.Exists(folderPath))
+                throw new DirectoryNotFoundException(string.Format("The directory was not found: {0}", folderPath));
+            this.Path = folderPath;
+            this.Files = Directory.GetFiles(this.Path);
+            IEnumerable<string> subFolders = Directory.GetDirectories(this.Path);
+            if (subFolders != null)
+            {
+                this.SubFolders = new List<Folder>();
+                foreach (var subFolder in subFolders)
+                    this.SubFolders.Add(new Folder(subFolder));
+            }
+        }
+
+        public XmlDocument GetFileTreeXmlWithThisFolderAsRoot()
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+
+            XmlNode root = xmlDocument.CreateElement("FileTree");
+            root.AppendChild(this.GetFolderNode(this, xmlDocument));
+            xmlDocument.AppendChild(root);
+
+            return xmlDocument;
+        }
+
+        private XmlNode GetFolderNode(Folder folder, XmlDocument xmlDocument)
+        {
+            XmlNode folderNode = xmlDocument.CreateElement("Folder");
+            XmlNode folderName = xmlDocument.CreateElement("Name");
+            folderName.InnerText = this.Name;
+            folderNode.AppendChild(folderName);
+
+            if (this.HasFiles)
+            {
+                XmlNode files = xmlDocument.CreateElement("Files");
+                foreach (string filePath in this.Files)
+                {
+                    XmlNode fileNode = xmlDocument.CreateElement("File");
+                    fileNode.InnerText = System.IO.Path.GetFileName(filePath);
+                    files.AppendChild(fileNode);
+                }
+                folderNode.AppendChild(files);
+            }
+            if (this.HasSubFolders)
+            {
+                XmlNode subfolders = xmlDocument.CreateElement("Subfolders");
+                foreach (Folder subFolder in this.SubFolders)
+                    subfolders.AppendChild(subFolder.GetFolderNode(subFolder, xmlDocument));
+                folderNode.AppendChild(subfolders);
+            }
+
+            return folderNode;
+        }
+    }
+}
---
+++ Tests/FileTreeTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileTree;
+using System.IO;
+using System.Xml;
+
+namespace Tests
+{
+    [TestClass]
+    public class FileTreeTest
+    {
+        public string RootfolderPath { get { return @"C:\CODE\TestData\FileTree\RootFolder"; } }
+
+        [TestMethod]
+        public void CanGetFileTree()
+        {
+            Folder rootFolder = new Folder(this.RootfolderPath);
+            Assert.AreEqual("RootFolder", rootFolder.Name);
+            Assert.IsTrue(rootFolder.HasSubFolders);
+            Assert.IsTrue(rootFolder.SubFolders.Count == 2);
+            Assert.IsTrue(rootFolder.HasFiles);
+            foreach (string file in rootFolder.Files)
+                Assert.IsTrue(Path.GetFileNameWithoutExtension(file) == "File1");
+            foreach (Folder subFolder in rootFolder.SubFolders)
+            {
+                if (subFolder.Name == "Subfolder1")
+                {
+                    Assert.IsFalse(subFolder.HasFiles);
+                    Assert.IsFalse(subFolder.HasSubFolders);
+                }
+                else if (subFolder.Name == "Subfolder2")
+                {
+                    Assert.IsFalse(subFolder.HasFiles);
+                    Assert.IsTrue(subFolder.HasSubFolders);
+                    Assert.IsTrue(subFolder.SubFolders.Count == 1);
+                    foreach (Folder subSubFolder in subFolder.SubFolders)
+                    {
+                        if (subSubFolder.Name == "Subfolder2-1")
+                        {
+                            Assert.IsFalse(subSubFolder.HasSubFolders);
+                            Assert.IsTrue(subSubFolder.HasFiles);
+                        }
+                        else
+                        {
+                            Assert.Fail("Folder is not: Subfolder2-1");
+                        }
+                    }
+                }
+                else
+                {
+                    Assert.Fail("Folder is not: Subfolder1 or Subfolder2");
+                }
+            }
+        }
+    }
+}
